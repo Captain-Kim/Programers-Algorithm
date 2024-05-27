ea3a9050bc746032223390e14eb6e80ced314ad1
Kim ByeongJun <160568904+Captain-Kim@users.noreply.github.com>
2024-05-27 09:45:29 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,15 @@
+function solution(my_strings, parts) {
+    var answer = '';
+    return answer;
+}
+
+function solution(my_strings, parts) {
+    let answer = '';
+    
+    for (let i = 0; i < my_strings.length; i++) {
+        let [s, e] = parts[i];
+        answer += my_strings[i].substring(s, e + 1);
+    }
+    
+    return answer;
+}
